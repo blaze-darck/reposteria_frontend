8e09172027a3053cbaa2039b3a5b4eebcba6db38
blaze-darck <paredesgabriel784@gmail.com>
2025-10-14 20:33:17 -0400

    feat: Adicion de vista de usuarios con tabla para visualizacion

---
+++ src/paginas/Usuarios.tsx
@@ -0,0 +1,11 @@
+import TablaUsuarios from "../componentes/usuarios/tabla_usuarios";
+
+function Usuarios() {
+  return (
+    <div>
+      <TablaUsuarios />
+    </div>
+  );
+}
+
+export default Usuarios;
