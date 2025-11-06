99e9a6d9b49ddcab4f19ed86c6c8f8e62c6f092d
blaze-darck <paredesgabriel784@gmail.com>
2025-11-05 20:52:10 -0400

    style:Cambio de colores principales

--- src/comun/vistarPaginaWeb/navbar.tsx
+++ src/comun/vistarPaginaWeb/navbar.tsx
@@ -49,7 +49,7 @@ const Navbar: React.FC = () => {
                   </div>
                   <button
                     onClick={handleLogout}
-                    className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition-all duration-300"
+                    className="px-4 py-2 bg-secundario text-texto rounded hover:bg-red-600 transition-all duration-300"
                   >
                     Cerrar sesión
                   </button>
