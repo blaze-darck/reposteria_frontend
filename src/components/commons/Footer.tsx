0e8005166a24435ad00b58e8a340987c5f2b49b5
blaze-darck <paredesgabriel784@gmail.com>
2025-12-06 09:42:45 -0400

    feat:Cambio en el footer

--- src/components/commons/Footer.tsx
+++ src/components/commons/Footer.tsx
@@ -6,28 +6,22 @@ export const Footer = () => {
     <footer className="py-16 bg-gray-950 px-12 flex justify-between gap-10 text-slate-200 text-sm flex-wrap mt-10 md:flex-nowrap">
       <Link
         to="/articulos"
-        className={`text-2xl font-bold tracking-tighter transition-all text-white flex-1`}
+        className="text-2xl font-bold tracking-tighter transition-all text-white flex-1"
       >
-        Wally Suma
+        Rosa Pastel
       </Link>
 
       <div className="flex flex-col gap-4 flex-1">
         <p className="font-semibold uppercase tracking-tighter">
           Correo Electronico
         </p>
-        <p className="text-xs font-medium">djdiegoguisbert@gmail.com</p>
+        <p className="text-xs font-medium">dilegabo@gmail.com</p>
       </div>
 
       <div className="flex flex-col gap-4 flex-1">
         <p className="font-semibold uppercase tracking-tighter">
           Datos a Poner
         </p>
-
-        <nav className="flex flex-col gap-2 text-xs font-medium">
-          <p>diego</p>
-          <p className="text-slate-300">nose que poner</p>
-          <p className="text-slate-300">:v</p>
-        </nav>
       </div>
 
       <div className="flex flex-col gap-4 flex-1">
